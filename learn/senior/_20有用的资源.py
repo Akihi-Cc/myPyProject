f40baaaec62988ffd189ab3e097c80eb629e641b
liuc <liuchao@bjca.org.cn>
2024-10-29 14:24:41 +0800

    docs(senior): 更新有用的资源文件
    - 新增多个 Python学习相关的网站链接
    - 添加多本 Python 相关的书籍推荐
    - 保留原有的文件头部注释

--- learn/senior/_20有用的资源.py
+++ learn/senior/_20有用的资源.py
@@ -1,1 +1,21 @@
 # !/usr/local/bin/python3.12
+
+# 相关网站
+# Python 3.6.3 中文手册         https://www.runoob.com/manual/pythontutorial3/docs/html/
+#
+# Python3 最新文档              https://docs.python.org/zh-cn/3/
+#
+# Python 2.X 版本的教程          https://www.runoob.com/python/python-tutorial.html
+#
+# Python 算法学习               https://github.com/TheAlgorithms/Python
+#
+# 相关书籍：
+# 父与子的编程之旅              https://item.jd.com/12741343.html?cu=true&utm_source=lianmeng__10__www.runoob.com&utm_medium=tuiguang&utm_campaign=t_23031_&utm_term=2057b6d655734f94afe0e70f3a849243
+#
+# Python 学习手册               https://item.jd.com/12452929.html?cu=true&utm_source=lianmeng__10__www.runoob.com&utm_medium=tuiguang&utm_campaign=t_23031_&utm_term=f2156de259ad4d65a9dc28020fe5afee
+#
+# Python编程 从入门到实践       https://item.jd.com/11993134.html?cu=true&utm_source=lianmeng__10__www.runoob.com&utm_medium=tuiguang&utm_campaign=t_23031_&utm_term=3f7abd554e4b40129bff2287b36cd988
+#
+# 利用Python进行数据分析        https://item.jd.com/12398725.html?cu=true&utm_source=lianmeng__10__www.runoob.com&utm_medium=tuiguang&utm_campaign=t_23031_&utm_term=8f6da28fc5504fd7b0b3bf154952050c
+#
+# 流畅的Python                 https://www.jyshare.com/books/
